2dadab77fc7f86c0ae00f2e2a5c2dbc3f52599f3
Mason <mason.bullock@ucalgary.ca>
2023-11-29 16:43:15 -0700

    Can grab workshop content from AWS

--- herai/src/pages/Events.js
+++ herai/src/pages/Events.js
@@ -1,5 +1,20 @@
+import { Document, Page } from "react-pdf";
+
 const Events = () => {
-    return <h1>EVENTS</h1>;
+    const videoSrc = "https://test-grab-bucket.s3.amazonaws.com/sample-5s.mp4";
+    const pdfSrc = "https://test-grab-bucket.s3.amazonaws.com/sample.pdf";
+
+    return (
+        <div>
+            <h1>EVENTS</h1>
+            <video width="320" height="240" controls>
+                <source src={videoSrc} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <iframe src={pdfSrc} width="90%" height="500px" />
+        </div>
+    );
 };
 
 export default Events;

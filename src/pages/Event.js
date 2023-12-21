2903e562eda20cb7066799e7b19568d7f6959886
Mason <mason.bullock@ucalgary.ca>
2023-12-21 16:04:12 -0700

    Video player

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,13 +1,35 @@
 import { useLocation, useParams } from "react-router-dom";
 import "../styles/event.css";
+import { useState } from "react";
+import VideoPane from "../components/VideoPane/VideoPane";
 
 const Event = () => {
     const location = useLocation();
     const event = location.state;
 
+    const [activeOption, setActiveOption] = useState("vid");
+
     if (event === null) {
         return <h1>NOT FOUND</h1>;
     }
+
+    const setView = () => {
+        if (activeOption === "vid") {
+            return (
+                <VideoPane
+                    src={
+                        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                    }
+                />
+            );
+        } else if (activeOption === "pdf") {
+            return <slidesPane />;
+        } else if (activeOption === "pic") {
+            return <galleryPane />;
+        } else if (activeOption === "info") {
+            return <detailsPane />;
+        }
+    };
 
     return (
         <div>
@@ -24,23 +46,36 @@ const Event = () => {
                 </div>
             </div>
             <div className="event-options">
-                <div className="active">
+                <div
+                    className={activeOption === "vid" ? "active" : ""}
+                    onClick={() => setActiveOption("vid")}
+                >
                     <img src="https://www.freeiconspng.com/thumbs/video-icon/video-icon-6.png"></img>
                     <h6>Video</h6>
                 </div>
-                <div>
+                <div
+                    className={activeOption === "pdf" ? "active" : ""}
+                    onClick={() => setActiveOption("pdf")}
+                >
                     <img src="https://cdn-icons-png.flaticon.com/512/6595/6595195.png"></img>
                     <h6>Slides</h6>
                 </div>
-                <div>
+                <div
+                    className={activeOption === "pic" ? "active" : ""}
+                    onClick={() => setActiveOption("pic")}
+                >
                     <img src="https://icons.iconarchive.com/icons/praveen/minimal-outline/512/gallery-icon.png"></img>
                     <h6>Gallery</h6>
                 </div>
-                <div>
+                <div
+                    className={activeOption === "info" ? "active" : ""}
+                    onClick={() => setActiveOption("info")}
+                >
                     <img src="https://static.thenounproject.com/png/2490660-200.png"></img>
                     <h6>Details</h6>
                 </div>
             </div>
+            {setView()}
         </div>
     );
 };

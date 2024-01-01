0b870240229630f6a56a8659b33e8812dc168db3
Mason <mason.bullock@ucalgary.ca>
2024-01-01 16:52:20 -0700

    about page mobile friendly design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import "./style.css";
 import { NavLink, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import React from "react";
 import logo from "../../img/logo.png";
 
 const Navbar = () => {
     const [isNavVisible, setNavVisible] = useState(false);
+    const navbarRef = useRef(null);
     const location = useLocation();
 
     useEffect(() => {
@@ -16,17 +17,24 @@ const Navbar = () => {
         setNavVisible(!isNavVisible);
     };
 
-    window.addEventListener("scroll", () => {
-        const navbar = document.querySelector("nav");
-        if (window.scrollY > 100) {
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    });
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                navbarRef.current.classList.add("scrolled");
+            } else {
+                navbarRef.current.classList.remove("scrolled");
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
-        <nav>
+        <nav ref={navbarRef}>
             <div className="mobile-menu">
                 <img id="nav-logo" src={logo} alt="not found"></img>
                 <button onClick={toggleNav} className="menu-icon">

8e64736f289bcf921bc1941fe23bb4a9c7945486
Mason <mason.bullock@ucalgary.ca>
2024-01-07 19:11:58 -0700

    basic page structure

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,23 @@
+import "../styles/profile.css";
+
 const Profile = () => {
     console.log("HERE");
-    return <h1>profile</h1>;
+    return (
+        <div>
+            <h1>Profile</h1>
+            <div>
+                <h6>Email: </h6>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="email@email.com"
+                ></input>
+                <button>Change</button>
+            </div>
+            <button>Delete Account</button>
+        </div>
+    );
 };
 
 export default Profile;

faadc0a5b5cbe4096d59f9ef49d432f7a19c7bca
Mason <mason.bullock@ucalgary.ca>
2024-01-10 00:02:50 -0700

    url fix for github pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { Amplify } from 'aws-amplify'
-import awsExports from './aws-exports';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import { HashRouter } from "react-router-dom";
 Amplify.configure(awsExports);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>
 );

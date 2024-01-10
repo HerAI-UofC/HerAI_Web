d7414ef5d65f4420790e7b5bcee6519f7fa95738
Mason <mason.bullock@ucalgary.ca>
2024-01-10 00:09:42 -0700

    nevermind

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import App from "./App";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
-import { HashRouter } from "react-router-dom";
 Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <App />
     </React.StrictMode>
 );

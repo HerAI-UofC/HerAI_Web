af6a7f8b398c095b47cca727014cb7d24b94f92f
Mason <mason.bullock@ucalgary.ca>
2024-01-09 23:12:37 -0700

    regular styling

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,15 +4,21 @@ import {
     fetchUserAttributes,
     updateUserAttribute,
     confirmUserAttribute,
+    deleteUser,
 } from "aws-amplify/auth";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+    const nav = useNavigate();
+
     const [userAttributes, setUserAttributes] = useState(null);
     const [newEmail, setNewEmail] = useState("");
     const [code, setCode] = useState("");
     const [codeFailed, setCodeFailed] = useState(false);
     const [verificationSent, setVerificationSent] = useState(false);
     const [updateFailed, setUpdateFailed] = useState(false);
+
+    const [askAgain, setAskAgain] = useState(false);
 
     const getUserAttributes = async () => {
         try {
@@ -73,6 +79,15 @@ const Profile = () => {
         }
     };
 
+    const deleteAccount = async () => {
+        try {
+            await deleteUser();
+            nav("/HerAI_Web");
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     useEffect(() => {
         getUserAttributes();
     }, []);
@@ -80,52 +95,82 @@ const Profile = () => {
     // console.log("HERE", userAttributes);
     // console.log("CODE", code);
     return (
-        <div>
-            <h1>Hello{userAttributes ? ", " + userAttributes.name : ""}!</h1>
-            <div>
-                <h6>Email: </h6>
-                <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    onChange={(e) => {
-                        setNewEmail(e.target.value);
-                        setVerificationSent(false);
-                        setCodeFailed(false);
-                    }}
-                    className={updateFailed ? "failed-form" : ""}
-                    placeholder={userAttributes ? userAttributes.email : ""}
-                ></input>
-                <button onClick={() => updateEmail("email", newEmail)}>
-                    Change
-                </button>
-            </div>
-            {updateFailed ? <p>Please enter a valid email</p> : null}
-            {verificationSent ? (
-                <div>
-                    <input
-                        type="text"
-                        id="code"
-                        name="code"
-                        placeholder="000000"
-                        className={codeFailed ? "failed-form" : ""}
-                        onChange={(e) => setCode(e.target.value)}
-                    ></input>
-                    <button
-                        onClick={() =>
-                            confirmNewEmail({
-                                userAttributeKey: "email",
-                                confirmationCode: code,
-                            })
-                        }
-                    >
-                        TEST CODE
-                    </button>
-                    {codeFailed ? <p>Incorrect Code</p> : null}
+        <div className="profile-body">
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"></img>
+            <div className="profile-text">
+                <h1>
+                    Hello{userAttributes ? ", " + userAttributes.name : ""}!
+                </h1>
+                <div className="email-section">
+                    <h6>Email: </h6>
+                    <div>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            onChange={(e) => {
+                                setNewEmail(e.target.value);
+                                setVerificationSent(false);
+                                setCodeFailed(false);
+                            }}
+                            className={updateFailed ? "failed-form" : ""}
+                            placeholder={
+                                userAttributes ? userAttributes.email : ""
+                            }
+                        ></input>
+                        {verificationSent ? (
+                            <div>
+                                <input
+                                    type="text"
+                                    id="code"
+                                    name="code"
+                                    placeholder="000000"
+                                    className={codeFailed ? "failed-form" : ""}
+                                    onChange={(e) => setCode(e.target.value)}
+                                ></input>
+                            </div>
+                        ) : null}
+                    </div>
+                    <div>
+                        <button onClick={() => updateEmail("email", newEmail)}>
+                            Change
+                        </button>
+                        {verificationSent ? (
+                            <div>
+                                <button
+                                    onClick={() =>
+                                        confirmNewEmail({
+                                            userAttributeKey: "email",
+                                            confirmationCode: code,
+                                        })
+                                    }
+                                >
+                                    Verify
+                                </button>
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
-            ) : null}
-
-            <button>Delete Account</button>
+                {updateFailed ? <p>Please enter a valid email</p> : null}
+                {codeFailed ? <p>Incorrect Code</p> : null}
+                <button id="delete-btn" onClick={() => setAskAgain(true)}>
+                    Delete Account
+                </button>
+                {askAgain ? (
+                    <div className="delete-section">
+                        <p>
+                            Are you sure you want to permanently delete your
+                            account, this action is irreversible
+                        </p>
+                        <div className="choice-section">
+                            <button onClick={() => deleteAccount()}>Yes</button>
+                            <button onClick={() => setAskAgain(false)}>
+                                No
+                            </button>
+                        </div>
+                    </div>
+                ) : null}
+            </div>
         </div>
     );
 };
